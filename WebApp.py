a74d5772fe1d80cd288e30b7c6774fa0bea315e0
sss <zln2017XD@aliyun.com>
2019-11-21 22:35:19 +0800

    add udthreads

--- WebApp.py
+++ WebApp.py
@@ -1,15 +1,15 @@
-import hashlib
 import json
-from textwrap import dedent
-from time import time
+import os
 from uuid import uuid4
+
 from flask import Flask
 from flask import jsonify
 from flask import request
-from BlockChain import BlockChain
-import socket
+from multiprocessing import Pool
+import os, time, random
 import udp
-import os
+from BlockChain import BlockChain
+import threading
 
 # 没写监听 广播加进去了 没写main 考虑用多进程
 
@@ -23,7 +23,7 @@ node_identifier = str(uuid4()).replace('-', '')
 blockchain = BlockChain()
 
 
-@app.route('/mine', methods=['GET'])
+# @app.route('/mine', methods=['GET'])
 def mine():
     block = blockchain.mine_pending_transaction(node_identifier)
     # We run the proof of work algorithm to get the next proof...
@@ -153,9 +153,40 @@ def receive(data):
 
 
 if __name__ == '__main__':
-    pid = os.fork()
-    if pid == 0:
-        print('app进程: I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
-    else:
-        print('监听进程: I (%s) just created a child process (%s).' % (os.getpid(), pid))
-        app.run(host='0.0.0.0', port=5000)
+
+    # print('Parent process {}.'.format(os.getpid()))
+    # p = Pool()
+    #
+    # p.apply(app.run, args=('0.0.0.0', 5000))
+    # p.apply(udp.listen)
+    #
+    # # for i in range(5):
+    # #     p.apply_async(long_time_task, args=(i,))
+    # print('Waiting for all subprocesses done...')
+    # p.close()
+    # p.join()
+    # print('All subprocesses done.')
+
+    locka = threading.Lock()
+    lockb = threading.Lock()
+    lockc = threading.Lock()
+
+    ta = threading.Thread(target=app.run, args=('0.0.0.0', 5000))
+    tb = threading.Thread(target=mine)
+    tc = threading.Thread(target=udp.listen)
+
+    locka.acquire()  # 保证a先执行
+
+    ta.start()
+    tb.start()
+    tc.start()
+
+    # ta.join()
+
+    #
+    # pid = os.fork()
+    # if pid == 0:
+    #     print('app进程: I am child process (%s) and my parent is %s.' % (os.getpid(), os.getppid()))
+    # else:
+    #     print('监听进程: I (%s) just created a child process (%s).' % (os.getpid(), pid))
+    #     # app.run(host='0.0.0.0', port=5000)
